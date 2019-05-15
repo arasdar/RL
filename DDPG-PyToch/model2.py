accb06fbdb3b841470f7ed9a0d8656400cb27118
arasdar <arasdar@uri.edu>
2019-05-15 15:50:50 -0400

    DDPG-PyTorch upgraded with BatchNorm layer.

--- DDPG-PyToch/model2.py
+++ DDPG-PyToch/model2.py
@@ -23,13 +23,17 @@ class Actor(nn.Module):
             fc2_units (int): Number of nodes in second hidden layer
         """
         super(Actor, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc1_units)
         
+        self.seed = torch.manual_seed(seed)
+        
+        self.fc1 = nn.Linear(state_size, fc1_units)
         self.bn1 = nn.BatchNorm1d(fc1_units)
         
         self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
+        
         self.fc3 = nn.Linear(fc2_units, action_size)
+        
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -40,8 +44,7 @@ class Actor(nn.Module):
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.bn1(self.fc1(state)))
-        
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.bn2(self.fc2(x)))
         return torch.tanh(self.fc3(x))
 
 
@@ -59,14 +62,17 @@ class Critic(nn.Module):
             fc2_units (int): Number of nodes in the second hidden layer
         """
         super(Critic, self).__init__()
+        
         self.seed = torch.manual_seed(seed)
         
         self.fcs1 = nn.Linear(state_size, fcs1_units)
-        
         self.bn1 = nn.BatchNorm1d(fcs1_units)
         
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
+        
         self.fc3 = nn.Linear(fc2_units, 1)
+        
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -79,5 +85,7 @@ class Critic(nn.Module):
         xs = F.relu(self.bn1(self.fcs1(state)))
         
         x = torch.cat((xs, action), dim=1)
-        x = F.relu(self.fc2(x))
+        
+        x = F.relu(self.bn2(self.fc2(x)))
+        
         return self.fc3(x)
