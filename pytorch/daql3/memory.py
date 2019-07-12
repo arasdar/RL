3a46a01a355e7c286c0ad62d00087ddbeb558c03
arasdar <arasdar@uri.edu>
2019-07-12 17:36:14 -0400

    DAQL3 is created for debugging.

---
+++ pytorch/daql3/memory.py
@@ -0,0 +1,46 @@
+import numpy as np
+import random
+import copy
+from collections import namedtuple, deque
+import torch
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Memory:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, buffer_size, batch_size, random_seed):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+        """
+        random.seed(random_seed)
+        
+        self.buffer = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.batch_size = batch_size
+        
+        #self.experience = namedtuple("exprience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.e = namedtuple("e", field_names=["s", "a", "r", "s2", "done"])
+    
+    def add(self, s, a, r, s2, done):
+        """Add a new experience (e) to memory."""
+        e_ = self.e(s, a, r, s2, done)
+        self.buffer.append(e_)
+    
+    def sample(self):
+        """Randomly sample a batch of experiences (E) from memory."""
+        E = random.sample(self.buffer, k=self.batch_size)
+
+        S = torch.from_numpy(np.vstack([e.s for e in E if e is not None])).float().to(device)
+        A = torch.from_numpy(np.vstack([e.a for e in E if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.r for e in E if e is not None])).float().to(device)
+        S2 = torch.from_numpy(np.vstack([e.s2 for e in E if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in E if e is not None]).astype(np.uint8)).float().to(device)
+
+        return (S, A, rewards, S2, dones)
+    
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.buffer)
---
+++ pytorch/daql3/modelD.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class D(nn.Module): # Discriminator
+    """Actor (policy) Model."""
+
+    def __init__(self, s_size, a_size, random_seed, h_size=400):
+        """Initialize parameters and build model.
+        Params
+        ======
+            s_size (int): Dimension of each state (s)
+            a_size (int): Dimension of each action (a)
+            random_seed (int): Random seed
+            h_size (int): Number of nodes in hidden layer
+        """
+        super(D, self).__init__()
+        
+        torch.manual_seed(random_seed)
+        
+        self.fc1 = nn.Linear(s_size, h_size)
+        
+        self.bn1 = nn.BatchNorm1d(h_size)
+        
+        self.fc2 = nn.Linear(h_size, a_size)
+        
+        self.init_parameters()
+
+    def init_parameters(self):
+        self.fc1.weight.data.uniform_(-3e-3, 3e-3) # normal (0, 1)
+        self.fc2.weight.data.uniform_(-3e-3, 3e-3) # normal (0, 1)
+        
+    def forward(self, S):
+        """Build an actor (policy) network that maps states (S) -> actions (A)."""
+        H = F.leaky_relu(self.bn1(self.fc1(S))) # H: hiddden layer/output
+
+        return torch.tanh(self.fc2(H)) # [-1, +1]
